958780b6d2dbc104cfe2bc3246bb53f96eab7e76
getsha <gertsha@gmail.com>
2022-05-13 14:34:14 +0300

    Added a search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt,response.data.weather[0].description");
 }
 
-let apiKey = "e187fb3f4af60991dba0543f601e6922";
-let city = "Nairobi";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "e187fb3f4af60991dba0543f601e6922";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Nairobi");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
